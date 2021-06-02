30218744b1748029caa9ded9318d3a18cacc0189
ericmbouwe <ericmbouwe@yahoo.fr>
2021-06-02 22:00:04 +0100

    add usefecth util

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import Home from './containers/Home';
 
 function App() {
-  return <div className="App">Student profiles</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <div>Home component</div>
+    </div>
+  );
+}
+
+export default Home;
