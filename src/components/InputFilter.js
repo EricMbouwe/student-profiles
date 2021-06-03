7a42d68ed14c5e214d6758d34a2725921b02500d
ericmbouwe <ericmbouwe@yahoo.fr>
2021-06-03 12:58:02 +0100

    add name filter component

--- src/components/InputFilter.js
+++ src/components/InputFilter.js
@@ -1,9 +1,32 @@
+import styled from 'styled-components';
+
 function InputFilter() {
   return (
     <div>
-      <h3>Input Filter</h3>
+      <InputField type="text" value="" placeholder="Search by name" />
     </div>
   );
 }
+
+const InputField = styled.input`
+  display: block;
+  width: 98%;
+  margin: 0 auto;
+  padding: 10px 5px;
+  border: none;
+  border-bottom: 2px solid #eee;
+  margin-bottom: 10px;
+  font-size: 18px;
+  
+  &:focus {
+    outline: none;
+    border-color: #aaa;
+  }
+  
+  ::placeholder {
+    font-size: 18px;
+    color: #aaa;
+  }
+`;
 
 export default InputFilter;
